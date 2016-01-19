ffff099a6be2c107d0e3cd1b46c1919eec6c7e46
Shannon Khoury <madkeeper@gmail.com>
2016-01-19 17:39:53 +1100

    Changes being made to recognise the block a card comes from inorder to reduce the reference card count creating a better chance of find the right card

--- MagicVision/Form1.cs
+++ MagicVision/Form1.cs
@@ -5,21 +5,16 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 using DirectX.Capture;
-using Microsoft.VisualBasic;
 using AForge;
 using AForge.Imaging;
 using AForge.Imaging.Filters;
 using AForge.Math.Geometry;
 using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace PoolVision
